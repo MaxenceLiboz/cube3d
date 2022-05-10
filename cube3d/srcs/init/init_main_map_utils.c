dba3ed588f12b2349defd06d4b9c92625ddea28e
mliboz <maxoulib@gmail.com>
2022-05-10 09:07:44 +0200

    Inside map error

--- cube3d/srcs/init/init_main_map_utils.c
+++ cube3d/srcs/init/init_main_map_utils.c
@@ -1,27 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init.h                                             :+:      :+:    :+:   */
+/*   init_main_map_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/06 11:50:38 by mliboz            #+#    #+#             */
-/*   Updated: 2022/05/10 08:56:31 by mliboz           ###   ########.fr       */
+/*   Created: 2022/05/10 08:11:55 by mliboz            #+#    #+#             */
+/*   Updated: 2022/05/10 08:57:51 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INIT_H
-# define INIT_H
+#include <cube3d.h>
 
-# include "structure.h"
-
-void	init_edition_map_var(t_prg *prg);
-void	init_edition_map_var2(t_prg *prg);
-bool	is_valid_map(t_prg *prg, int player_check);
-bool	init_map(t_prg *prg);
-void	init_mini_map(t_prg *prg);
-bool	window_init(t_win *window);
-bool	img_init(t_win window, t_data *img);
-bool	is_valid_map_char(int position);
-
-#endif
+bool	is_valid_map_char(int position)
+{
+	if (position != 'N' && position != 'S' && position != 'E' && position != 'W'
+		&& position != ' ')
+		return (false);
+	return (true);
+}
